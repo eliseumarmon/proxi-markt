261bc8e63d62aceb9f58b75be049cd2bcd2f66d1
Eliseu <elimarmon@alu.edu.gva.es>
2025-12-19 13:33:29 +0100

    posats tots els tipos dels camps en majúscules

--- docker/database/base.sql
+++ docker/database/base.sql
@@ -1,12 +1,12 @@
 USE proxi_markt;
 
 CREATE TABLE USUARIOS(
-    id_usuario int AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre_usuario VARCHAR(255) UNIQUE NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    contrasenya varchar(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    contrasenya VARCHAR(255) NOT NULL,
     telefono VARCHAR(20) UNIQUE NOT NULL, 
-    direccion varchar(255),
+    direccion VARCHAR(255),
     longitud DECIMAL(10,8),
     latitud DECIMAL(10,8),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -15,30 +15,30 @@ CREATE TABLE USUARIOS(
 );
 
 CREATE TABLE CATEGORIAS(
-    id_categoria int AUTO_INCREMENT PRIMARY KEY,
-    nombre_categoria varchar(255) NOT NULL 
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(255) NOT NULL 
 );
 
 CREATE TABLE PRODUCTOS(
-    id_producto int AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
     nombre_producto VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10,2) NOT NULL,
-    stock_total int NOT NULL DEFAULT 0,
-    stock_reserva int NOT NULL DEFAULT 0,
-    stock_real int NOT NULL DEFAULT 0,
+    stock_total INT NOT NULL DEFAULT 0,
+    stock_reserva INT NOT NULL DEFAULT 0,
+    stock_real INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     imagen VARCHAR(255),
-    id_categoria int,
+    id_categoria INT,
     estado ENUM('agotado', 'reservado', 'disponible'), 
     FOREIGN KEY (id_categoria) REFERENCES CATEGORIAS(id_categoria)
 );
 
-CREATE TABLE CHAT(
-    id_chat int AUTO_INCREMENT PRIMARY KEY,
-    id_comprador int,
-    id_vendedor int,
-    id_producto int,
+CREATE TABLE CHATS(
+    id_chat INT AUTO_INCREMENT PRIMARY KEY,
+    id_comprador INT,
+    id_vendedor INT,
+    id_producto INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_comprador) REFERENCES USUARIOS(id_usuario),
@@ -48,17 +48,17 @@ CREATE TABLE CHAT(
 );
 
 CREATE TABLE MENSAJES(
-    id_mensaje int AUTO_INCREMENT PRIMARY KEY,
-    id_chat int,
-    id_envio int,
+    id_mensaje INT AUTO_INCREMENT PRIMARY KEY,
+    id_chat INT,
+    id_envio INT,
     contenido TEXT NOT NULL,
     FOREIGN KEY (id_chat) REFERENCES CHAT(id_chat),
     FOREIGN KEY (id_envio) REFERENCES USUARIOS(id_usuario)
 );
 
 CREATE TABLE PUNTOS_ENTREGA(
-    id_punto int AUTO_INCREMENT PRIMARY KEY,
-    id_usuario int,
+    id_punto INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
     longitud DECIMAL(10,8) NOT NULL,
     latitud DECIMAL(10,8) NOT NULL,
     nombre_punto VARCHAR(255) NOT NULL,
@@ -69,12 +69,12 @@ CREATE TABLE PUNTOS_ENTREGA(
 );
 
 CREATE TABLE COMPRAVENTAS(
-    id_compraventa int AUTO_INCREMENT PRIMARY KEY,
-    id_producto int,
-    id_comprador int,
-    id_vendedor int,
-    cantidad_total int NOT NULL,
-    id_punto int,
+    id_compraventa INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    id_comprador INT,
+    id_vendedor INT,
+    cantidad_total INT NOT NULL,
+    id_punto INT,
     estado ENUM('pendiente', 'en curso', 'completado', 'cancelado'),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_producto) REFERENCES PRODUCTOS(id_producto),
@@ -84,10 +84,10 @@ CREATE TABLE COMPRAVENTAS(
 );
 
 CREATE TABLE VALORACIONES(
-    id_valoracion int AUTO_INCREMENT PRIMARY KEY,
+    id_valoracion INT AUTO_INCREMENT PRIMARY KEY,
     id_venta INT,
-    id_resenyador int,
-    id_resenyado int,
+    id_resenyador INT,
+    id_resenyado INT,
     valoracion ENUM('1','2','3','4','5') NOT NULL,
     comentario TEXT,
     fecha DATETIME DEFAULT CURRENT_TIMESTAMP, 
