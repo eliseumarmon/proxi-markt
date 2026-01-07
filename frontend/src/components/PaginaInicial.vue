f21be5bb71a172e55fc7bc4474d5b7f201c18de7
David Camarena <davidcugamer23@gmail.com>
2026-01-07 13:38:22 +0100

    Perfil i pagina inicial

---
+++ frontend/src/components/PaginaInicial.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+    const irALogin = () => {
+        router.push('/login')
+    }
+
+    const irARegistro = () => {
+        router.push('/register')
+    }
+</script>
+<template>
+    <div class="elementos">
+        <h2>Bienvenido a Proximarket</h2>
+        <div class="botones">
+            <button @click="irALogin">Login</button>
+            <button @click="irARegistro">Registrarse</button>
+        </div>
+    </div>
+
+</template>
