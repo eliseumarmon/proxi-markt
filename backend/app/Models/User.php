cbcb7189570fe14914270983f735d022644e7f7c
Jordi <jograuan@gmail.com>
2026-01-08 10:36:37 +0100

    Get,Post,Controlador y modelo de usaurios

---
+++ backend/app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'usuraios';
+    protected $fillable = [
+        'nombre_usuario',
+        'email',
+        'contrasenya',
+        'telefono',
+    ];
+}
